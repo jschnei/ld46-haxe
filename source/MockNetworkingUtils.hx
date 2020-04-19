327cd50215db43b73f46eff6ca5b7f5d04f4ccd1
jon <noemail>
2020-04-19 16:55:24 -0400

    add mocknetworkingutils.hx; if you want to build on neko, swap this with networkingutils

---
+++ source/MockNetworkingUtils.hx
@@ -0,0 +1,14 @@
+//import haxe.net.WebSocket;
+
+class NetworkingUtils {
+
+    public static function test() 
+    {
+        
+    }
+
+    public static function sendMessage(type: String, message: String)
+    {
+        
+    }
+}
