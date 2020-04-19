f7031371da5a97d991930d35f1fa30416a4079b5
Anderson Wang <anderson.wang1994@gmail.com>
2020-04-19 02:03:33 -0400

    change dimensions to 5x10

--- source/Registry.hx
+++ source/Registry.hx
@@ -6,6 +6,8 @@ import polygonal.ds.IntHashSet;
 
 class Registry
 {
+    public static var PLAYFIELD_WIDTH = 5;
+    public static var PLAYFIELD_HEIGHT = 10;
     public static var GRID_SIZE = 64;
     public static var FALL_SPEED:Int = 200;
     public static var letterFreq:Array<Float> = [0.08167,0.01492,0.02202,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.01292,0.04025,0.02406,0.06749,0.07507,0.01929,0.0095,0.05987,0.06327,0.09356,0.02758,0.00978,0.02560,0.0015,0.01994,0.00077];
