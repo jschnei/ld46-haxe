7e45882fe1f944badc4a8948525f3c0e25f5d0b2
jon <noemail>
2020-04-19 23:01:16 -0400

    change server from localhost to 0.0.0.0

--- server/server.py
+++ server/server.py
@@ -90,7 +90,7 @@ async def counter(websocket, path):
         await unregister(websocket)
 
 
-start_server = websockets.serve(counter, "localhost", 9999)
+start_server = websockets.serve(counter, "0.0.0.0", 9999)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
