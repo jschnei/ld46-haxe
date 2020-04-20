a7cae1a309630561ea89ff5562f2d493c1d15cfb
Anderson Wang <anderson.wang1994@gmail.com>
2020-04-20 18:13:29 -0400

    change minigrid to make 60 small sprites instead, hopefully this fixes the lag issue

--- source/MiniGrid.hx
+++ source/MiniGrid.hx
@@ -3,20 +3,20 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
 import flixel.system.FlxSound;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
 
-
-class MiniGrid extends FlxSprite
+class MiniGrid extends FlxSpriteGroup
 {
     public var playState:PlayState;
 
     public static var CELL_WIDTH:Int = 16;
     public static var CELL_HEIGHT:Int = 16;
-    public static var MARGIN:Int = 0;
+    public static var MARGIN:Int = 1;
 
     public var gridHeight:Int;
     public var gridWidth:Int;
@@ -29,6 +29,9 @@ class MiniGrid extends FlxSprite
     public var updateTimer:Float = 0;
     public static var UPDATE_FREQ:Float = 1.5;
 
+    public var gridSprite:FlxSprite;
+    public var letterSprites:Array<FlxSprite>;
+
     public function new(playState:PlayState, width:Int, height:Int, ?X:Float=0, ?Y:Float=0)
     {
         super(X, Y);
@@ -37,10 +40,33 @@ class MiniGrid extends FlxSprite
 
         gridWidth = width;
         gridHeight = height;
+        
+        gridSprite = new FlxSprite();
+        gridSprite.makeGraphic(gridWidth*CELL_WIDTH+1, 
+                               gridHeight*CELL_HEIGHT+1, 
+                               FlxColor.TRANSPARENT, true);
+        add(gridSprite);
 
-        makeGraphic(gridWidth*CELL_WIDTH+1, 
-                    gridHeight*CELL_HEIGHT+1, 
-                    FlxColor.TRANSPARENT, true);
+        letterSprites = new Array<FlxSprite>();
+        for(y in 0...gridHeight)
+        {
+            for(x in 0...gridWidth)
+            {
+                var letterSprite:FlxSprite = new FlxSprite(x*CELL_WIDTH + MARGIN, y*CELL_HEIGHT + MARGIN);
+                letterSprite.makeGraphic(CELL_WIDTH - MARGIN, CELL_HEIGHT - MARGIN, FlxColor.WHITE);
+                letterSprites.push(letterSprite);
+                add(letterSprite);
+            }
+        }
+
+        for(x in 0...gridWidth+1)
+        {
+            FlxSpriteUtil.drawLine(gridSprite, x*CELL_WIDTH, 0, x*CELL_WIDTH, gridHeight*CELL_HEIGHT, {color: 0xffffffff, thickness: 3});  
+        }
+        for(y in 0...gridHeight+1)
+        {
+            FlxSpriteUtil.drawLine(gridSprite, 0, y*CELL_HEIGHT, gridWidth*CELL_WIDTH, y*CELL_HEIGHT, {color: 0xffffffff,thickness: 3});
+        }
 
         var emptyBoardString = "";
         for (i in 0...gridWidth*gridHeight) emptyBoardString += "0";
@@ -65,6 +91,7 @@ class MiniGrid extends FlxSprite
                 trace("not tracking anything");
             }
         }
+        super.update(elapsed);
     }
 
     public function trackPlayer(playerInfo:PlayerInfo)
@@ -80,35 +107,18 @@ class MiniGrid extends FlxSprite
         {
             for(x in 0...gridWidth)
             {
-                if (boardString.charAt(gridWidth * y + x) == "1")
+                var index = gridWidth * y + x;
+                if (boardString.charAt(index) == "1")
                 {
-                    FlxSpriteUtil.drawRect(this, 
-                                           x*CELL_WIDTH + MARGIN, 
-                                           y*CELL_HEIGHT + MARGIN, 
-                                           CELL_WIDTH - MARGIN, 
-                                           CELL_HEIGHT - MARGIN,
-                                           FlxColor.YELLOW);
+                    letterSprites[index].color = FlxColor.RED;
                 }
                 else
                 {
-                    FlxSpriteUtil.drawRect(this, 
-                                            x*CELL_WIDTH + MARGIN, 
-                                            y*CELL_HEIGHT + MARGIN, 
-                                            CELL_WIDTH - MARGIN, 
-                                            CELL_HEIGHT - MARGIN,
-                                            FlxColor.BLACK);
+                    letterSprites[index].color = FlxColor.BLACK;
                 }
             }
         }
 
-        for(x in 0...gridWidth+1)
-        {
-            FlxSpriteUtil.drawLine(this, x*CELL_WIDTH, 0, x*CELL_WIDTH, gridHeight*CELL_HEIGHT, {color: 0xffffffff, thickness: 3});  
-        }
-        for(y in 0...gridHeight+1)
-        {
-            FlxSpriteUtil.drawLine(this, 0, y*CELL_HEIGHT, gridWidth*CELL_WIDTH, y*CELL_HEIGHT, {color: 0xffffffff,thickness: 3});
-        }
     }
 
 }
