91204d17bfe527714a9b4c21a3811f49fa1b5503
Anderson Wang <anderson.wang1994@gmail.com>
2020-04-20 21:55:34 -0400

    add press P text, also move text around

--- source/WaitingState.hx
+++ source/WaitingState.hx
@@ -19,6 +19,8 @@ class WaitingState extends FlxState
     var _lobby_users:FlxText;
     var _lobby_users_label:FlxText;
 
+    var _help_label:FlxText;
+
     var update_lobby_timer:Float = 0;
     public static var UPDATE_LOBBY_FREQ = 0.5;
 	
@@ -33,24 +35,29 @@ class WaitingState extends FlxState
         
         NetworkingUtils.initialize();
 
-        _name_label = new FlxText(200, 250);
+        _name_label = new FlxText(200, 150);
         _name_label.setFormat(AssetPaths.Fira_Bold__ttf, 30, FlxColor.ORANGE, FlxTextAlign.LEFT);
         _name_label.text = "Name: " + Registry.curGame.myNickname;
         add(_name_label);
 
-        _room_label = new FlxText(200, 300);
+        _room_label = new FlxText(200, 200);
         _room_label.setFormat(AssetPaths.Fira_Bold__ttf, 30, FlxColor.ORANGE, FlxTextAlign.LEFT);
         _room_label.text = "Room: " + NetworkingUtils.room;
         add(_room_label);
 
-        _lobby_users_label = new FlxText(200, 350);
+        _lobby_users_label = new FlxText(200, 250);
         _lobby_users_label.setFormat(AssetPaths.Fira_Bold__ttf, 30, FlxColor.ORANGE, FlxTextAlign.LEFT);
         _lobby_users_label.text = "Players: ";
         add(_lobby_users_label);
 
-        _lobby_users = new FlxText(200, 400);
+        _lobby_users = new FlxText(200, 300);
         _lobby_users.setFormat(AssetPaths.Fira_Bold__ttf, 30, FlxColor.RED, FlxTextAlign.LEFT);
         add(_lobby_users);
+
+        _help_label = new FlxText(500, 575);
+        _help_label.setFormat(AssetPaths.Fira_Bold__ttf, 20, FlxColor.WHITE, FlxTextAlign.LEFT);
+        _help_label.text = "When everyone is ready,\npress P to play!";
+        add(_help_label);
 
 		super.create();
 	}	
